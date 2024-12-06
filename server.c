7d6f8f3afcc37e9bda1a3928ff6f0e8363ebc60f
cornwarecjp <cornwarecjp@ultimatestunts.nl>
2024-12-06 14:52:45 +0100

    Add simple TCP listener code

--- server.c
+++ server.c
@@ -19,10 +19,99 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define CONNECTIONQUEUELEN 10
+#define PORTNUMBER 5001
+
+
+
+void serve(int fd)
+{
+	char buffer[256];
+	int  n;
+
+	while(1)
+	{
+		bzero(buffer,256);
+		n = read(fd, buffer, 255);
+
+		if(n < 0)
+		{
+			perror("ERROR in reading from socket");
+			exit(1);
+		}
+
+		printf("client said: %s \n", buffer);
+
+		n = write(fd, buffer, strlen(buffer));
+
+		if(n < 0)
+		{
+			perror("ERROR in writing to socket");
+			exit(1);
+		}
+
+		if(!memcmp(buffer, "quit", 4))
+			break;
+	}
+	
+	close(fd);
+}
+
+
+void listenOnPort(int port, void (*callback)(int))
+{
+	int sockfd, newsockfd, clilen;
+	struct sockaddr_in serv_addr, cli_addr;
+	const int enable = 1;
+
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd < 0)
+	{
+		perror("Couldn't open socket\n");
+		exit(1);
+	}
+
+	if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+	{
+		perror("setsockopt(SO_REUSEADDR) failed\n");
+		exit(1);
+	}
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(PORTNUMBER);
+
+	if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	{
+		perror("ERROR on binding\n");
+		exit(1);
+	}
+
+	listen(sockfd, CONNECTIONQUEUELEN);
+
+	while(1)
+	{
+		clilen = sizeof(cli_addr);
+		newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+		callback(newsockfd);
+	}
+}
+
 
 int main(int argc, char **argv)
 {
-	printf("Server\n");
+	listenOnPort(PORTNUMBER, serve);
 	return 0;
 }
 
